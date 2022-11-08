378fd6d8fcf3cc70b87a45dac6233a5552e6af76
Marco Antonio Caldeira <caldeiran391@gmail.com>
2022-11-08 09:15:46 -0300

    meu primeiro commit

---
+++ Exercicio01.py
@@ -0,0 +1,13 @@
+num1 = int(input('Digite um numero: '))
+
+num2 = int(input('Digite o segundo numero: '))
+
+soma = num1 + num2;
+divisao = num1 / num2;
+multi = num1 * num2;
+sub = num1 - num2;
+
+print('Operações aritimeticas entre:  ', num1 ,'e', num2, 'são ', soma, divisao, multi, sub)
+
+
+
