40a252d0647afb74ef56ad138ba3e07a108429f2
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-01-16 13:12:51 -0300

    Menipulando listas

---
+++ Lista_de_Vetores/exercicio_01.py
@@ -0,0 +1,5 @@
+numeros = [1, 2, 3, 4, 5]
+numeros.append(6)
+cont = 0
+
+print(numeros)
--- Visão-Geral/Manipulando_Listas.py
+++ Visão-Geral/Manipulando_Listas.py
@@ -9,5 +9,22 @@ lista = [123, True, 'Marco Antonio', 1.2, []]
 
 # Listas são a mesma coisa que vetores
 
-print(lista)
-print(lista[2])
+# print(lista)
+# print(lista[2])
+
+# Manipulando listas de maneira avançada
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8]
+
+lista.append("Marco")  # Adicionei meu nome na lista
+nome = lista.pop()
+del lista[-1]
+
+lista.insert(100, 5)
+print(lista[4])
+
+lista_a = ['Marco', 'Luiz', 2, 3, 6]
+lista_b = lista_a.copy()
+
+lista_a[0] = 'Qualquer coisa'
+print(lista_b)
