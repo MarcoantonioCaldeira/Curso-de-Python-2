04fc29486d68ec08db52aa682b21d02a10928c5a
Marco Antonio <caldeiran391@gmail.com>
2023-01-19 21:41:16 -0300

    Adicionando Exercicios

---
+++ .history/Lista_de_Vetores/Exercicio05_20230119211044.py
@@ -0,0 +1,8 @@
+num = []
+cont_num_par = 0
+cont_num_impar = 0
+
+for cont in 10:
+
+    num = int(input("Digite um numero: "))
+    cont_num_par += 1
---
+++ .history/Lista_de_Vetores/Exercicio05_20230119211958.py
@@ -0,0 +1,19 @@
+NUMEROS = 20
+par = []
+impar = []
+num_inteiro = []
+
+
+for cont in range(NUMEROS):
+
+    num = int(input("Digite um numero: "))
+    num_inteiro.append(num)
+
+    if num % 2 == 0:
+        par.append(num)
+    else:
+        impar.append(num)
+
+print("Numeros Inteiros")
+for num in num_inteiro:
+    print(num, "\n")
